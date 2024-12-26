a861196571a3814a7839f26f9c1f27e17ce2557d
RIPSolidSn@kELaptop\Snake <reuter.alican@gmail.com>
2024-12-26 16:06:56 +0100

    Added drag & drop fuction for TaskPanel

--- Sudoku/src/UI/Controls/Panels/TaskPanel.cs
+++ Sudoku/src/UI/Controls/Panels/TaskPanel.cs
@@ -7,6 +7,8 @@ using static Shared.Configs.UI.Controls;
 namespace UI.Controls.Panels;
 internal class TaskPanel : Panel {
 	internal PanelType panelType;
+	private bool isDragging;
+	private Point start = new(0, 0);
 	internal TaskPanel(PanelType type) {
 		this.panelType = type;
 		InitializeControl();
@@ -16,11 +18,27 @@ internal class TaskPanel : Panel {
 		this.BackColor = Color.Transparent;
 		this.Size = new Size(TaskPanelWidth, TaskPanelHeight);
 		this.Location = new Point(0, 0);
+		this.MouseDown += new MouseEventHandler(OnMouseDown);
+		this.MouseUp += new MouseEventHandler(OnMouseUp);
+		this.MouseMove += new MouseEventHandler(OnMouseMove);
 		AddChilds();
 	}
 	private void AddChilds() {
 		this.Controls.Add(new TaskButton(ButtonType.TaskBarClose, 1));
 		this.Controls.Add(new TaskButton(ButtonType.TaskBarMaximize, 2));
 		this.Controls.Add(new TaskButton(ButtonType.TaskBarMinimize, 3));
+	}
+	private void OnMouseDown(object sender, MouseEventArgs e) {
+		isDragging = true;
+		start = e.Location;
+	}
+	private void OnMouseUp(object sender, MouseEventArgs e) {
+		isDragging = false;
+		start = new(0, 0);
+	}
+	private void OnMouseMove(object sender, MouseEventArgs e) {
+		if (!isDragging) { return; }
+		this.Parent.Left += e.X - start.X;
+		this.Parent.Top += e.Y - start.Y;
 	}
 }
